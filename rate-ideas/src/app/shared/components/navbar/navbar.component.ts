79833add8c1647d0613690422d78c24602f1f364
jasurbek <jasurbekibnxusniddin@gmail.com>
2025-02-26 23:12:58 +0500

    primay button

--- rate-ideas/src/app/shared/components/navbar/navbar.component.ts
+++ rate-ideas/src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
     selector: 'app-navbar',
+    imports: [MatButtonModule],
     templateUrl: './navbar.component.html',
 })
 
--- rate-ideas/tailwind.config.js
+++ rate-ideas/tailwind.config.js
@@ -6,10 +6,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        'primary': '#3490dc',
+        'primary': '#F2F4F7',
         'secondary': '#ffed4a',
         'danger': '#e3342f',
-      }
+      },
+      textColor: {
+        'primary': '#1D2939',
+      },
+      borderColor: {
+        'primary': '#E4E7EC',
+      },
     },
   },
   plugins: [],
