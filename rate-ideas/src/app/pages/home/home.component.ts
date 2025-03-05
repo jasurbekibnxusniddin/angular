2004a26489bea67fec1e5590bdcf4b524934fc77
jasurbek <jasurbekibnxusniddin@gmail.com>
2025-03-05 22:25:22 +0500

    add components

--- rate-ideas/src/app/pages/home/home.component.ts
+++ rate-ideas/src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from "@angular/core";
 import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
 import { TabComponent } from "../../shared/components/tab/tab.component";
 import { TabDataI } from "../../shared/models/frontend/tab";
+import { AccordionListComponent } from "../../shared/components/accordion-list/accordion-list.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  imports: [NavbarComponent, TabComponent],
+  imports: [NavbarComponent, TabComponent, AccordionListComponent],
 })
 
 export class HomeComponent {
---
+++ rate-ideas/src/app/shared/components/accordion-list/components/accordion-item/accordion-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion-item',
+  imports: [],
+  templateUrl: './accordion-item.component.html',
+  styles: ``
+})
+export class AccordionItemComponent {
+
+}
